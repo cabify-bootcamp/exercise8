f4c860a69b533c421b34460357a38128e0adc907
mfgomezb <mgomezb89@gmail.com>
2018-11-12 17:35:39 +0100

    added /haproxy and modified healthchecks

--- message/controllers/getHealth.js
+++ message/controllers/getHealth.js
@@ -1,10 +1,8 @@
 require('dotenv').config();
 
-const port = process.env.SERVICE_PORT
 
 module.exports = function(req, res) {
     
-    Promise.resolve(res.status(200).send('ok').then(()=>{
-        console.log(`Healthy service on port: ${port}`)
-    }))
+        res.send('status: 200')
+
 };
